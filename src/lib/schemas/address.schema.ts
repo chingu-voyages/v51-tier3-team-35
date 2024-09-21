9806be2490fc066f6b4606bc5b35af916bad7079
David Eastmond <davebuck1981@gmail.com>
2024-09-21 10:22:36 -0400

    Address model and schema

---
+++ src/lib/schemas/address.schema.ts
@@ -0,0 +1,28 @@
+import { Schema } from "mongoose";
+import { Address } from "../models/address.model";
+
+export const addressSchema = new Schema<Address>(
+  {
+    street: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    zip: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
