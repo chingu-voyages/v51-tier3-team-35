504a8e349997cf60f2a6d556866ec007da32c2c0
David Eastmond <davebuck1981@gmail.com>
2024-09-25 09:06:29 -0400

    installed the big calendar and playground

--- src/app/adventure/view/[adventureId]/page.tsx
+++ src/app/adventure/view/[adventureId]/page.tsx
@@ -1,15 +1,22 @@
 "use client";
+import dayjs from "dayjs";
 import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { Calendar, dayjsLocalizer, SlotInfo } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import { Adventure } from "../../../../lib/models/adventure.model";
 import { AdventureService } from "../../../services/adventure-service";
 
 export default function ViewEditAdventurePage() {
   const params = useParams<{ adventureId: string }>();
+  const [currentDate, setCurrentDate] = useState(dayjs("2024-09-22"));
   const [adventure, setAdventure] = useState<Adventure | null>(null);
+
   useEffect(() => {
     fetchAdventureById();
   }, []);
+
+  const localizer = dayjsLocalizer(dayjs);
 
   const fetchAdventureById = async () => {
     try {
@@ -24,11 +31,65 @@ export default function ViewEditAdventurePage() {
     }
   };
 
+  const handleSelectEvent = useCallback((event: any) => {
+    console.log(event);
+  }, []);
+
+  const handleSelectSlot = useCallback((slotInfo: SlotInfo) => {
+    console.log(slotInfo);
+  }, []);
+
+  const handleNavigate = (date: Date, view: string, action: string) => {
+    if (action === "NEXT") {
+      if (view === "week") {
+        setCurrentDate(dayjs(currentDate).add(1, "week"));
+      } else {
+        setCurrentDate(dayjs(currentDate).add(1, "month"));
+      }
+    } else if (action === "PREV") {
+      if (view === "week") {
+        setCurrentDate(dayjs(currentDate).subtract(1, "week"));
+      } else {
+        setCurrentDate(dayjs(currentDate).subtract(1, "month"));
+      }
+    }
+  };
+
   // This is a placeholder to get basic functionality working
   return (
     <div>
       <h1>View edit an adventure with id {params.adventureId}</h1>
       <h1>{adventure?.description}</h1>
+      <div>
+        <Calendar
+          date={currentDate.toDate()}
+          defaultView="week"
+          localizer={localizer}
+          events={[
+            {
+              _id: "3",
+              title: "Direct Object",
+              start: dayjs().hour(4).minute(0).toDate(),
+              end: dayjs().hour(5).minute(0).toDate(),
+            },
+            {
+              title: "Test2",
+              start: dayjs().add(1, "day").hour(4).minute(0).toDate(),
+              end: dayjs().add(1, "day").hour(5).minute(0).toDate(),
+            },
+          ]}
+          onNavigate={(date, view, action) => {
+            // Navigate back or next one week at a time
+            handleNavigate(date, view, action);
+          }}
+          onSelectEvent={handleSelectEvent}
+          onSelectSlot={handleSelectSlot}
+          onView={(view) => console.log(view)}
+          className=""
+          style={{ height: 800 }}
+          selectable
+        />
+      </div>
     </div>
   );
 }
