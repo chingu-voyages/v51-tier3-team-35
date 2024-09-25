c1ef82fcdde05b78a96f8524494ddc4f99178f84
David Eastmond <davebuck1981@gmail.com>
2024-09-25 09:55:32 -0400

    playground - drag-create events are working locally

--- src/app/adventure/view/[adventureId]/page.tsx
+++ src/app/adventure/view/[adventureId]/page.tsx
@@ -1,17 +1,39 @@
 "use client";
 import dayjs from "dayjs";
+import mongoose from "mongoose";
 import { useParams } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
-import { Calendar, dayjsLocalizer, SlotInfo } from "react-big-calendar";
+import { Calendar, dayjsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { Adventure } from "../../../../lib/models/adventure.model";
 import { AdventureService } from "../../../services/adventure-service";
 
+interface EventOccurrenceData {
+  _id: string;
+  title: string;
+  start: Date;
+  end: Date;
+}
 export default function ViewEditAdventurePage() {
   const params = useParams<{ adventureId: string }>();
   const [currentDate, setCurrentDate] = useState(dayjs("2024-09-22"));
   const [adventure, setAdventure] = useState<Adventure | null>(null);
-
+  const [eventOccurrences, setEventOccurrences] = useState<
+    EventOccurrenceData[]
+  >([
+    {
+      _id: "3",
+      title: "Direct Object",
+      start: dayjs().hour(4).minute(0).toDate(),
+      end: dayjs().hour(5).minute(0).toDate(),
+    },
+    {
+      _id: "4",
+      title: "Test2",
+      start: dayjs().add(1, "day").hour(4).minute(0).toDate(),
+      end: dayjs().add(1, "day").hour(5).minute(0).toDate(),
+    },
+  ]);
   useEffect(() => {
     fetchAdventureById();
   }, []);
@@ -35,9 +57,23 @@ export default function ViewEditAdventurePage() {
     console.log(event);
   }, []);
 
-  const handleSelectSlot = useCallback((slotInfo: SlotInfo) => {
-    console.log(slotInfo);
-  }, []);
+  const handleSelectSlot = useCallback(
+    ({ start, end }: { start: Date; end: Date }) => {
+      // This will need to send a put request eventualy, but just testing
+      setEventOccurrences((prev) => {
+        return [
+          ...prev,
+          {
+            _id: new mongoose.Types.ObjectId().toString(),
+            title: "New Event",
+            start,
+            end,
+          },
+        ];
+      });
+    },
+    []
+  );
 
   const handleNavigate = (date: Date, view: string, action: string) => {
     if (action === "NEXT") {
@@ -65,19 +101,7 @@ export default function ViewEditAdventurePage() {
           date={currentDate.toDate()}
           defaultView="week"
           localizer={localizer}
-          events={[
-            {
-              _id: "3",
-              title: "Direct Object",
-              start: dayjs().hour(4).minute(0).toDate(),
-              end: dayjs().hour(5).minute(0).toDate(),
-            },
-            {
-              title: "Test2",
-              start: dayjs().add(1, "day").hour(4).minute(0).toDate(),
-              end: dayjs().add(1, "day").hour(5).minute(0).toDate(),
-            },
-          ]}
+          events={eventOccurrences}
           onNavigate={(date, view, action) => {
             // Navigate back or next one week at a time
             handleNavigate(date, view, action);
