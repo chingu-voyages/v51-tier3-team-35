96633a34c9d6425d16003a2f1ec8132838018a4c
Brian Stasiukaitis <brianstas@gmail.com>
2024-09-26 17:58:40 -0400

    Added static page and editable page
    Have the editing state now so that it swaps between the edit form and the regular display of user information.

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -16,6 +16,7 @@ export default function UserProfile({params}:{ params: { userId: string }}){
     console.log("id on user profile is: ", id);
     
     const [user, setUser] = useState<Partial<User>>({});
+    const [isEditing, setIsEditing] = useState(false);
 
 
     useEffect(() =>{
@@ -24,10 +25,15 @@ export default function UserProfile({params}:{ params: { userId: string }}){
     console.log("User now looks like: ", user);
     },[])
 
+    const initialFormValues: FormValues={
+        name: user?.name || '',
+        password: '',
+      }
+
     if (!user) {
         return <div>Loading...</div>;
       }
-    
+    console.log("isEditing? ", isEditing);
     
       const handleSubmit = async (values: FormValues) => {
         await updateUserProfile(values.name, values.password, id);
@@ -36,12 +42,36 @@ export default function UserProfile({params}:{ params: { userId: string }}){
       return (
         <div className="max-w-4xl mx-auto p-6">
           <h1 className="text-2xl font-semibold mb-4">User Profile</h1>
+
+          {!isEditing ? (
+            <div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium">Name</label>
+              <p className="mt-1 text-gray-700">{user.name}</p>
+            </div>
+  
+            <div className="mb-4">
+              <label className="block text-sm font-medium">Email</label>
+              <p className="mt-1 text-gray-700">{user.email}</p>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-sm font-medium">Password</label>
+              <p className="mt-1 text-gray-700">••••••••</p>
+            </div>
+  
+            <button
+              onClick={() => setIsEditing(true)} // Switch to edit mode
+              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            >
+              Edit
+            </button>
+          </div>
+          ):(
     
           <Formik
-            initialValues={{
-              name: user?.name || '',
-              password: '',
-            }}
+            initialValues={initialFormValues}
+            enableReinitialize
             onSubmit={handleSubmit}
           >
             {({ isSubmitting }) => (
@@ -91,6 +121,7 @@ export default function UserProfile({params}:{ params: { userId: string }}){
               </Form>
             )}
           </Formik>
+          )}
         </div>
       );
 }
