9bbc4f24130fa54db303950a19e3acad20462702
David Eastmond <davebuck1981@gmail.com>
2024-09-26 22:35:01 -0400

    auto-complete component

---
+++ src/components/address-autocomplete/Address-autocomplete.tsx
@@ -0,0 +1,42 @@
+import { usePlacesWidget } from "react-google-autocomplete";
+
+interface AddressAutoCompleteProps {
+  onPlaceSelected?: (place: google.maps.places.PlaceResult) => void;
+}
+
+export function AddressAutoComplete(props: AddressAutoCompleteProps) {
+  const { ref } = usePlacesWidget({
+    apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    options: {
+      types: ["geocode", "establishment"],
+    },
+    onPlaceSelected: (place) => {
+      props.onPlaceSelected && props.onPlaceSelected(place);
+    },
+  });
+
+  return (
+    <>
+      <label className="input input-bordered flex items-center gap-2">
+        <input
+          type="text"
+          className="grow"
+          placeholder="Search"
+          ref={ref as any}
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          className="h-4 w-4 opacity-70"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </label>
+    </>
+  );
+}
