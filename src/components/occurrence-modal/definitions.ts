070dda13ec9ef991a5c8193623fea6764b37dba6
David Eastmond <davebuck1981@gmail.com>
2024-09-27 13:42:32 -0400

    typedefs

---
+++ src/components/occurrence-modal/definitions.ts
@@ -0,0 +1,28 @@
+import { TravelOccurrenceMethod } from "../../lib/models/occurrence.model";
+
+export interface TravelOccurrenceSubmitData {
+  travelMethod: TravelOccurrenceMethod;
+  startLocation: google.maps.places.PlaceResult;
+  endLocation: google.maps.places.PlaceResult | null;
+}
+
+export interface AccommodationOccurrenceSubmitData {
+  location: google.maps.places.PlaceResult;
+  checkIn: Date;
+  checkOut: Date;
+  accommodationName: string;
+}
+
+export interface ActivityOccurrenceSubmitData {
+  location: google.maps.places.PlaceResult;
+  title: string;
+}
+
+export interface FoodOccurrenceSubmitData
+  extends ActivityOccurrenceSubmitData {}
+
+export type OccurrenceSubmissionData =
+  | TravelOccurrenceSubmitData
+  | AccommodationOccurrenceSubmitData
+  | ActivityOccurrenceSubmitData
+  | FoodOccurrenceSubmitData;
