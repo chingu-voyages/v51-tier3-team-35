4284d8ada7436db1818eb53d975d69378e65def0
David Eastmond <davebuck1981@gmail.com>
2024-09-27 13:42:51 -0400

    google address autocomplete competition

--- src/components/address-autocomplete/Address-autocomplete.tsx
+++ src/components/address-autocomplete/Address-autocomplete.tsx
@@ -1,5 +1,5 @@
 import { usePlacesWidget } from "react-google-autocomplete";
-
+import { IoIosSearch } from "react-icons/io";
 interface AddressAutoCompleteProps {
   onPlaceSelected?: (place: google.maps.places.PlaceResult) => void;
 }
@@ -24,18 +24,7 @@ export function AddressAutoComplete(props: AddressAutoCompleteProps) {
           placeholder="Search"
           ref={ref as any}
         />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="h-4 w-4 opacity-70"
-        >
-          <path
-            fillRule="evenodd"
-            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-            clipRule="evenodd"
-          />
-        </svg>
+        <IoIosSearch className="text-[30px]" />
       </label>
     </>
   );
