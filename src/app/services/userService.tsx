fa647d1d91975ae991e87e4277e77d80b154335d
Brian Stasiukaitis <brianstas@gmail.com>
2024-09-27 21:56:02 -0400

    added UserService validation
    UserService patch will only trigger for name if the password doesn't have a length. Otherwise, it will send both.

--- src/app/services/userService.tsx
+++ src/app/services/userService.tsx
@@ -12,6 +12,8 @@ export const fetchUserProfile = async (userId: string): Promise<Partial<User>> =
 };
 
 export const updateUserProfile = async (name: string, password: string, userId: string) => {
+    
+    if(password.length > 0){
     const response = await fetch(`http://localhost:3000/api/users/${userId}`, {
         method: 'PATCH',
         headers: { 'Content-Type': 'application/json' },
@@ -19,6 +21,15 @@ export const updateUserProfile = async (name: string, password: string, userId: 
     });
     if (!response.ok) throw new Error('Error updating profile');
     return response.json();
+    } else {
+        const response = await fetch(`http://localhost:3000/api/users/${userId}`, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name }),
+        });
+        if (!response.ok) throw new Error('Error updating profile');
+        return response.json(); 
+    }
 };
 
 export const fetchUsers = async(): Promise<Partial<User>[]> =>{
