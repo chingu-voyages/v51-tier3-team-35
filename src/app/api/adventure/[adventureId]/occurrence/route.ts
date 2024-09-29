0367d774026a95daa23e4b20e9d339bc6dfbe159
David Eastmond <davebuck1981@gmail.com>
2024-09-29 19:43:34 -0400

    update api stuff

--- src/app/api/adventure/[adventureId]/occurrence/route.ts
+++ src/app/api/adventure/[adventureId]/occurrence/route.ts
@@ -2,29 +2,35 @@ import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import dbConnect from "../../../../../lib/mongodb/mongodb";
 import { AdventureModel } from "../../../../../lib/schemas/adventure.schema";
+import authOptions from "../../../auth/auth-options";
+import { OccurrencePutRequestHandlerFactory } from "../../../lib/occurrence-put-request-handler-factory";
 
 export async function PUT(
   req: NextRequest,
   { params }: { params: { adventureId: string } }
 ) {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
   if (!session) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
   const requestBody = await req.json();
-  console.info(requestBody);
 
   await dbConnect();
   try {
     // Find the adventure, make sure it exists
-    const adventure = await AdventureModel.findById(params.adventureId);
-    if (!adventure) {
+    const adventureDocument = await AdventureModel.findById(params.adventureId);
+    if (!adventureDocument) {
       return NextResponse.json(
         { error: `Adventure with id ${params.adventureId} not found` },
         { status: 404 }
       );
     }
+
+    // Handle the request
+    await OccurrencePutRequestHandlerFactory.create(
+      requestBody.eventType
+    ).handle(session.user!._id, adventureDocument, requestBody);
 
     return NextResponse.json({ message: "Success" }, { status: 200 });
   } catch (error: any) {
