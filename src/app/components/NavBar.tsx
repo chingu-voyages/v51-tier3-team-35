5488cd2a96ee62bd1a5438f497ee36ab45b2962d
David Eastmond <davebuck1981@gmail.com>
2024-10-10 15:52:04 -0400

    NavBar updates

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,35 +1,38 @@
-'use client'
-import Link from "next/link";
+"use client";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 
-export default function NavBar(){
+export default function NavBar() {
   const { data: session, status } = useSession();
 
-    return (
-        <nav className="navbar bg-base-100 shadow-md">
-          <div className="container mx-auto flex justify-between items-center">
-
-            <div className="flex items-center">
-              <Link href="/" className="text-2xl font-bold">
-                Adventure Canvas
-              </Link>
-            </div>
-            {status === "authenticated" ? 
-              <div className="flex space-x-4">
-                <Link href="/adventure/new" className="btn btn-primary">
-                  Start New Canvas
-                </Link>
-                <Link href={`users/${session?.user?._id}`} className="btn btn-outline">
-                  My Profile
-                </Link>
-              </div> : 
-              <div className="flex space-x-4">
-                <Link href="/signin" className="btn btn-outline">
-                  Sign In
-                </Link>
-              </div>}
-  
+  return (
+    <nav className="navbar bg-base-100 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex items-center">
+          <Link href="/" className="text-2xl font-bold">
+            Adventure Canvas
+          </Link>
+        </div>
+        {status === "authenticated" ? (
+          <div className="flex space-x-4">
+            <Link href="/adventure/new" className="btn btn-primary">
+              New Adventure
+            </Link>
+            <Link
+              href={`users/${session?.user?._id}`}
+              className="btn btn-outline"
+            >
+              My Profile
+            </Link>
           </div>
-        </nav>
-      );
+        ) : (
+          <div className="flex space-x-4">
+            <Link href="/signin" className="btn btn-outline">
+              Sign In
+            </Link>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 }
