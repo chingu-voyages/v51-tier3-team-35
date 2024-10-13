1b209f0e2c9169bf5ec7b34936ef7b8455d6e373
David Eastmond <davebuck1981@gmail.com>
2024-10-12 23:23:18 -0400

    comments stacker component

---
+++ src/components/comment-components/Comment-stacker.tsx
@@ -0,0 +1,63 @@
+import dayjs from "dayjs";
+import { useState } from "react";
+import { StackerComment } from "../../lib/models/occurrence.model";
+
+interface CommentsContainerProps {
+  comments: StackerComment[];
+  onSubmit: (comment: string) => void;
+}
+export function CommentsContainer({
+  comments,
+  onSubmit,
+}: CommentsContainerProps) {
+  const [commentText, setCommentText] = useState("");
+  return (
+    <>
+      <h4 className="font-bold text-xl mt-4">Comments</h4>
+      <div className="p-2">
+        <div
+          id="comments-container"
+          className={` ${
+            comments?.length > 0 ? "h-[200px]" : ""
+          } overflow-auto`}
+        >
+          {comments &&
+            comments.map((comment) => (
+              <Comment comment={comment} key={comment._id} />
+            ))}
+        </div>
+        <div id="comments-maker" className="w-full flex gap-x-2 mt-2">
+          <input
+            type="text"
+            placeholder="Type a comment here..."
+            className="input input-bordered w-full"
+            value={commentText}
+            onChange={(e) => setCommentText(e.target.value)}
+            maxLength={100}
+          />
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              onSubmit && onSubmit(commentText);
+              setCommentText("");
+            }}
+          >
+            Post
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+const Comment = ({ comment }: { comment: StackerComment }) => {
+  return (
+    <div className="flex flex-col p-2">
+      <p className="font-bold italic">{comment.name} wrote:</p>
+      <div className="flex justify-between">
+        <p>{comment.text}</p>
+        <p>{dayjs(comment.createdAt).format("MMM DD YYYY, HH:MM")}</p>
+      </div>
+    </div>
+  );
+};
