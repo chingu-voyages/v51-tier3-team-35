c6a034561b33536ada22d0a4525d504d9344d375
David Eastmond <davebuck1981@gmail.com>
2024-10-13 09:12:59 -0400

    linting

--- src/app/services/userService.tsx
+++ src/app/services/userService.tsx
@@ -1,34 +1,37 @@
 import { User } from "../../lib/models/user.model";
 
-export const fetchUserProfile = async (userId: string): Promise<Partial<User>> => {
-    const response = await fetch(`http://localhost:3000/api/users/${userId}`, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-    });
-    if (!response.ok) throw new Error('Error fetching user profile');
-    const data = await response.json();
-    console.log("data from fetch looks like: ", data)
-    return data.user;
+export const fetchUserProfile = async (
+  userId: string
+): Promise<Partial<User>> => {
+  const response = await fetch(`http://localhost:3000/api/users/${userId}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+  if (!response.ok) throw new Error("Error fetching user profile");
+  const data = await response.json();
+  return data.user;
 };
 
-export const updateUserProfile = async (name: string, password: string, userId: string) => {
-    
-    if(password.length > 0){
+export const updateUserProfile = async (
+  name: string,
+  password: string,
+  userId: string
+) => {
+  if (password.length > 0) {
     const response = await fetch(`http://localhost:3000/api/users/${userId}`, {
-        method: 'PATCH',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name, password }),
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, password }),
     });
-    if (!response.ok) throw new Error('Error updating profile');
+    if (!response.ok) throw new Error("Error updating profile");
     return response.json();
-    } else {
-        const response = await fetch(`http://localhost:3000/api/users/${userId}`, {
-            method: 'PATCH',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ name }),
-        });
-        if (!response.ok) throw new Error('Error updating profile');
-        return response.json(); 
-    }
+  } else {
+    const response = await fetch(`http://localhost:3000/api/users/${userId}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name }),
+    });
+    if (!response.ok) throw new Error("Error updating profile");
+    return response.json();
+  }
 };
-
